fcda52bb474794aeeeeaf3ada514aec03408aecd
Lucas Detling <lucas.detling@gmail.com>
2024-03-13 09:25:59 +0100

    disponibility on ourBooks

--- views/templates/ourbooks.php
+++ views/templates/ourbooks.php
@@ -13,14 +13,17 @@
         <?php
         // ajouter les bons liens 
         foreach ($books as $book) {
-            echo "<a href='index.php?action=showBook&id=" . $book["id"] . "' class='card-book'>";
-            echo "  <img src='upload/books/" . $book["img"] . "' alt='image du livre " . $book["title"] . " '>";
-            echo "  <div class='text-book'>";
-            echo "      <h3>" . $book["title"] . "</h3>";
-            echo "      <div class='book-author'>" . $book["author"] . "</div>";
-            echo "      <div class='book-seller'>vendu par : " . $book["pseudo"] . "</div>";
-            echo "  </div>";
-            echo "</a>";
+        ?>
+            <a href='index.php?action=showBook&id=<?= $book["id"] ?>' class='card-book'>
+                <img src='upload/books/<?= $book["img"] ?>' alt='image du livre <?= $book["title"] ?>'>
+                <?= !$book["disponibility"] ? "<div class='disponibility'>Non dispo.</div>" : null ?>
+                <div class='text-book'>
+                    <h3><?= $book["title"] ?></h3>
+                    <div class='book-author'><?= $book["author"] ?></div>
+                    <div class='book-seller'>vendu par : <?= $book["pseudo"] ?></div>
+                </div>
+            </a>
+        <?php
         }
         ?>
     </div>
